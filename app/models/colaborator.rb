47c1a2f414d4ab60556ff3480b13412c70a8bab9
Brayan Diaz C <brayandiazc@gmail.com>
2023-06-30 10:38:16 -0500

    feat: Estilos

--- app/models/colaborator.rb
+++ app/models/colaborator.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: colaborators
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  email      :string
+#  byo        :text
+#  available  :boolean          default(TRUE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Colaborator < ApplicationRecord
 end
--- test/models/colaborator_test.rb
+++ test/models/colaborator_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: colaborators
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  email      :string
+#  byo        :text
+#  available  :boolean          default(TRUE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class ColaboratorTest < ActiveSupport::TestCase
